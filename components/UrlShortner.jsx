cb4503f5210828a611c2aa76f16329ab4b0155e7
MJ-1112 <m22327803@gmail.com>
2025-12-17 19:15:34 +0530

    edit2

--- components/UrlShortner.jsx
+++ components/UrlShortner.jsx
@@ -17,7 +17,7 @@ export default function UrlShortener() {
     setShortUrl("");
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/shorten`, {
+      const res = await fetch('https://miniurl-2maq.onrender.com/api/shorten', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
